c937baf7ade14f1c09f89ef0ed8bdfac310945c3
ChamudithaML <lilan.2019660@iit.ac.lk>
2024-09-16 02:57:06 +0530

    fix class name issue

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,66 +1,61 @@
-import React from 'react'
-import './Footer.css'
-
+import React from 'react';
+import './Footer.css';
 import { Link } from 'react-router-dom';
 
 function Footer() {
     return (
         <div className='footer-container'>
-            <section class='social-media'>
-                <div class='social-media-wrap'>
-
-                    <div class='footer-logo'>
+            <section className='social-media'>
+                <div className='social-media-wrap'>
+                    <div className='footer-logo'>
                         <Link to='/' className='social-logo'>
                             CassiaShield
-                            <i class='fas fa-coffee' />
+                            <i className='fas fa-coffee' />
                         </Link>
                     </div>
-
-                    <small class='author'>By Lilan Chamuditha (2024)</small>
-
-                    <div class='social-icons'>
+                    <small className='author'>By Lilan Chamuditha (2024)</small>
+                    <div className='social-icons'>
                         <Link
-                            class='social-icon-link facebook'
+                            className='social-icon-link facebook'
                             to='/'
                             target='_blank'
                             aria-label='Facebook'
                         >
-                            <i class='fab fa-facebook-f' />
+                            <i className='fab fa-facebook-f' />
                         </Link>
                         <Link
-                            class='social-icon-link instagram'
+                            className='social-icon-link instagram'
                             to='/'
                             target='_blank'
                             aria-label='Instagram'
                         >
-                            <i class='fab fa-instagram' />
+                            <i className='fab fa-instagram' />
                         </Link>
                         <Link
-                            class='social-icon-link youtube'
+                            className='social-icon-link youtube'
                             to='/'
                             target='_blank'
                             aria-label='Youtube'
                         >
-                            <i class='fab fa-youtube' />
+                            <i className='fab fa-youtube' />
                         </Link>
                         <Link
-                            class='social-icon-link twitter'
+                            className='social-icon-link twitter'
                             to='/'
                             target='_blank'
                             aria-label='Twitter'
                         >
-                            <i class='fa-brands fa-x-twitter' />
+                            <i className='fa-brands fa-x-twitter' />
                         </Link>
                         <Link
-                            class='social-icon-link twitter'
+                            className='social-icon-link linkedin'
                             to='/'
                             target='_blank'
                             aria-label='LinkedIn'
                         >
-                            <i class='fab fa-linkedin' />
+                            <i className='fab fa-linkedin' />
                         </Link>
                     </div>
-
                 </div>
             </section>
         </div>
